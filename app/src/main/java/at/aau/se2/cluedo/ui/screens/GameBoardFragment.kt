e33ff115e440bd68f509a28ba844384d0193d4ab
Portar <ds@mscg.at>
2025-05-03 13:26:04 +0200

    Adding a new Gameboard grid and implementing the movement of the Char

--- app/src/main/java/at/aau/se2/cluedo/ui/screens/GameBoardFragment.kt
+++ app/src/main/java/at/aau/se2/cluedo/ui/screens/GameBoardFragment.kt
@@ -7,16 +7,29 @@ import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.Nullable
-import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
 class GameBoardFragment(context: Context) : View(context) {
+
+    //Bitmaps
     private var playerBitmap: Bitmap? = null
     private var gameBoardBitmap:Bitmap? = null
+    private var grid:Bitmap? = null
+
+    //Player and Board variables
     private var playerX: Float = 0f
     private var playerY: Float = 0f
-    private var boardX:Float = 0f
-    private var boardY:Float = 0f
+    private var boardPosX:Float = 0f
+    private var boardPosY:Float = 0f
+    private var sizeX:Int=1000
+    private var sizeY:Int=1000
+    private var playerPosX=24
+    private var playerPosY=24
+    private var playerSizeX=sizeX/25
+    private var playerSizeY=sizeY/25
+
+    //Button
+
 
     constructor(context: Context, @Nullable attrs: AttributeSet?) : this(context) {
         init()
@@ -28,14 +41,20 @@ class GameBoardFragment(context: Context) : View(context) {
 
     private fun init() {
         println("HI")
-        playerBitmap = BitmapFactory.decodeResource(resources, R.drawable.chess_game)
+        playerBitmap = BitmapFactory.decodeResource(resources, R.drawable.block)
         gameBoardBitmap = BitmapFactory.decodeResource(resources, R.drawable.gameboard)
+        gameBoardBitmap = BitmapFactory.decodeResource(resources, R.drawable.grid)
         // Set initial position (example: center of the view)
-        playerBitmap= playerBitmap?.let { Bitmap.createScaledBitmap(it,60,60,false) }
-        gameBoardBitmap = gameBoardBitmap?.let { Bitmap.createScaledBitmap(it,700,700,false) }
+        playerBitmap= playerBitmap?.let { Bitmap.createScaledBitmap(it,playerSizeX,playerSizeY,false) }
+        gameBoardBitmap = gameBoardBitmap?.let { Bitmap.createScaledBitmap(it,sizeX,sizeY,false) }
+        grid = grid?.let { Bitmap.createScaledBitmap(it,sizeX,sizeY,false) }
         gameBoardBitmap?.let {
-            boardX =  (it.width - width) / 2f
-            boardY = (it.height - height) / 2f
+            boardPosX =  0f//(it.width - width) / 2f
+            boardPosY = 0f //(it.height - height) / 2f
+        }
+        grid?.let {
+            boardPosX =  0f//(it.width - width) / 2f
+            boardPosY = 0f //(it.height - height) / 2f
         }
         playerBitmap?.let {
             playerX = (width - it.width) / 2f
@@ -54,13 +73,16 @@ class GameBoardFragment(context: Context) : View(context) {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        playerX = 400f
-        playerY = 700f
-
         gameBoardBitmap?.let {
-            canvas.drawBitmap(it,0f,0f,null)
+           canvas.drawBitmap(it,boardPosX,boardPosY,null)
+            playerX =((playerPosX*(sizeX/25))+boardPosX)+2
+            playerY = ((playerPosY*(sizeY/25))+boardPosY)
+        }
+        grid?.let {
+            canvas.drawBitmap(it,boardPosX,boardPosY,null)
         }
         playerBitmap?.let {
+
             canvas.drawBitmap(it, playerX, playerY, null) // Draw the bitmap
 
         }
